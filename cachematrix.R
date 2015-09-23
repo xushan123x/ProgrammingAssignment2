b7e66541239ef0eb8d2c7b334aba2ec848733b0c
unknown <297432905@qq.com>
2015-09-24 00:07:54 +0800

    finish function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCeMatrix will return a list including four functions which can:
+#  1 set the value of the matrix
+#  2 get the value of the matrix
+#  3 set the value of the inversion
+#  4 get the value of the inversion
 
-## Write a short comment describing this function
+## The following function ceSolve calculates the inversion of the special "matrix" created 
+#  with the above function. However, it first checks to see if the inversion has already been 
+#  calculated. If so,it gets the inversion from cache and skips the computation. Otherwise,
+#  it calculates the inversion of the data and sets the value of the inversion in the cache  
+#  via the setinverse function.
 
-makeCacheMatrix <- function(x = matrix()) {
+##########################################################################
+
+makeCeMatrix <- function(x = matrix()) {
+  n <- NULL
+  set <- function(y){
+    x<<-y
+    n<<-NULL
+  }
+  get<-function()x
+  setinverse<-function(inverse) n<<-inverse
+  getinverse<-function() n
+  list(set=set,get=get,getinverse=getinverse,setinverse=setinverse)
 
 }
 
+## Function ceSolve calculates the inversion of the special "matrix" created 
+#  with the above function. However, it first checks to see if the inversion has already been 
+#  calculated. If so,it gets the inversion from cache and skips the computation. Otherwise,
+#  it calculates the inversion of the data and sets the value of the inversion in the cache  
+#  via the setinverse function.
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+ceSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mn<-x$getinverse()
+  if(!is.null(n)){
+    message("getting ced data")
+    return (n)
+  }
+  data<-x$get()
+  n<-solve(data,...)
+  x$setinverse(n)
+  n
 }
+
+
+###################################################################
+
